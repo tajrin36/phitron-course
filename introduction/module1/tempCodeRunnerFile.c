858f08b4d2d50a7d0275d107e5e91192c10dccc1
husna_tajrin <husnajahantajrin@gmail.com>
2025-04-07 12:38:56 +0600

    basic c programmimg

---
+++ introduction/module1/tempCodeRunnerFile.c
@@ -0,0 +1,25 @@
+//! print function
+    // printf("hello world!\n");
+
+    // //! variable
+    // int a = 2;
+    // printf("num: %d\n",a);
+
+    // //! data type & format specifier
+    // int num1 = 10;
+    // float num2 = 20.30;
+    // char ch = 'h';
+    // printf("integer number: %d\n",num1); 
+    // printf("float number: %f\n",num2); 
+    // printf("character: %c\n",ch);
+    
+    // //! type casting
+    // printf("float number: %f\n",(float)num1); 
+    // printf("integer number: %d\n",(int)num2); 
+    // printf("integer number: %d\n",(int)ch); 
+
+    // //! boolean data type
+    // bool bool1 = true;
+    // bool bool2 = false;
+    // printf("boolean true value: %d\n",bool1);
+    // printf("boolean false value: %d\n",bool2);
